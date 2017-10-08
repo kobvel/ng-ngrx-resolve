4c2bc5f508a34cbef7211514503e8eac43a69620
Mykhailo Churilov <m.churilov@svitla.com>
2017-10-08 12:27:25 +0300

    edited readme

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,6 @@
-import { IProfileData } from './profile.model';
-import { OnInit, OnDestroy, Component } from '@angular/core';
-import { Subscription } from 'rxjs/Rx';
-import { ApiService } from '../api.service';
+import { OnInit, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProfileData } from './profile.model';
 
 @Component({
   selector: 'app-profile',
